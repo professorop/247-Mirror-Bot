326c69bccb3043eda60e4e3e266a45515d74ce1e
Rk Shaju 612 <91930059+professorop@users.noreply.github.com>
2022-06-08 21:01:40 +0600

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -402,7 +402,7 @@ def _mirror(bot, message, isZip=False, extract=False, isQbit=False, isLeech=Fals
             mega_dl = MegaDownloader(listener)
             Thread(target=mega_dl.add_download, args=(link, f'{DOWNLOAD_DIR}{listener.uid}/')).start()
         else:
-            sendMessage('❖ MEGA Disabled..! /n❖ Gib MEGA premium wen?', bot, message)
+            sendMessage('❖ MEGA Disabled..!, Gib MEGA premium wen?', bot, message)
     elif isQbit:
         qb_dl = QbDownloader(listener)
         Thread(target=qb_dl.add_qb_torrent, args=(link, f'{DOWNLOAD_DIR}{listener.uid}', qbitsel)).start()
